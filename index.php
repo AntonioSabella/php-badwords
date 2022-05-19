c385cefe99d3420e485eee2cee0c74484b8a838f
AntonioSabella <asabella6@gmail.com>
2022-05-19 15:30:31 +0200

    Add text and censored version

--- index.php
+++ index.php
@@ -8,6 +8,16 @@ dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola d
 
 <?php 
 // Codice in Php
+//1. Creo variabile con paragrafo di testo
+$my_text = "Io sono un testo piccino picciò che apparirà a schermo, perdincibacco!";
+
+//3. Creo variabile che conterrà la parola passata dall'utente che sarà censurata
+$user_word = $_GET['user_word'];
+var_dump($user_word);
+
+//4. Censuriamo la porzione di testo che contiene la parola inserita
+$censor_text = str_replace($user_word, '***', $my_text);
+var_dump($censor_text);
 ?>
 
 <!DOCTYPE html>
@@ -20,5 +30,12 @@ dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola d
 </head>
 <body>
     <!-- MarkUp -->
+    <h1 style='text-align: center; color: cornflowerblue; font-size: 50px'>Badwords</h1>
+    <!-- 2. Stampo a schermo il testo e la sua lunghezza -->
+    <p style='text-align: center; font-size: 20px; color: red; font-weight: 500'>Questo è il mio testo: "<?php echo $my_text; ?>" ed ha una lunghezza di: "<?php echo strlen($my_text); ?>" lettere </p>
+    <!-- 5. Stampo a schermo la barola bannata, il testo censurato e la sua lunghezza -->
+    <p style='text-align: center; font-size: 20px;'>La parola bannata è : <span style='color: red; font-weight: 600'>"<?php echo $user_word; ?>"</span> </p>
+    <p style='text-align: center; font-size: 20px; color: green; font-weight: 500'>Questo è il mio testo family friendly: "<?php echo $censor_text ?>" ed ha una lunghezza di: "<?php echo strlen($censor_text); ?>" lettere </p>
+
 </body>
 </html>
